542e40cebfa2d6012688ae2cf515e4e2130360c7
Igor Rončević <ironcev@hotmail.com>
2015-01-16 22:13:08 +0100

    Social sharing href urls prettified.

--- assets/scripts/site.js
+++ assets/scripts/site.js
@@ -39,7 +39,8 @@ $(function () {
             && !obj.href.match(/^mailto\:/)
             && !(obj.hostname == location.hostname)
             && !$(obj).hasClass("twitter")
-            && !$(obj).hasClass("googlePlus");
+            && !$(obj).hasClass("googlePlus")
+            && !$(obj).hasClass("facebook");
     };
 
     $("a:external").addClass("new-window");
@@ -53,9 +54,24 @@ $(function () {
     $(".twitter, .googlePlus").click(function (event) {
         var anchor = $(event.currentTarget);
         var width = 600, height = 600;
-        var url = stringFormat(anchor.attr("href"),
+        var url = stringFormat(anchor.attr("href") + anchor.attr("data-href"),
                                encodeURIComponent(anchor.attr("data-text")),
                                encodeURIComponent(anchor.attr("data-url")));
+        var left = (window.screen.width / 2) - (width / 2);
+        var top = (window.screen.height / 2) - (height / 2);
+        window.open(url, "",
+                    stringFormat('toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width={0},height={1},top={2},left={3}', width, height, top, left));
+        event.preventDefault();
+    });
+
+    // Share on Facebook.
+    $(".facebook").click(function (event) {
+        var anchor = $(event.currentTarget);
+        var width = 600, height = 600;
+        var url = stringFormat(anchor.attr("href") + anchor.attr("data-href"),
+                               encodeURIComponent(anchor.attr("data-link")),
+                               encodeURIComponent(anchor.attr("data-picture")),
+                               encodeURIComponent(anchor.attr("data-description")));
         var left = (window.screen.width / 2) - (width / 2);
         var top = (window.screen.height / 2) - (height / 2);
         window.open(url, "",

ad6fd4d781b14de1238cbb47a247b9fb70e7d2de
Igor Rončević <ironcev@hotmail.com>
2015-12-25 20:47:38 +0100

    Dead injection of Piwik script removed.

--- assets/scripts/site.js
+++ assets/scripts/site.js
@@ -19,20 +19,6 @@ $(function () {
         activeOverlay: false,
     });
 
-    // Piwik.
-    var _paq = _paq || [];
-    _paq.push(["setCookieDomain", "*.thehumbleprogrammer.com"]);
-    _paq.push(["setDomains", ["*.thehumbleprogrammer.com"]]);
-    _paq.push(['trackPageView']);
-    _paq.push(['enableLinkTracking']);
-    (function () {
-        var u = (("https:" == document.location.protocol) ? "https" : "http") + "://piwik.t-hp.me/";
-        _paq.push(['setTrackerUrl', u + 'piwik.php']);
-        _paq.push(['setSiteId', 1]);
-        var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0]; g.type = 'text/javascript';
-        g.defer = true; g.async = true; g.src = u + 'piwik.js'; s.parentNode.insertBefore(g, s);
-    })();
-
     // Open external links in a new window.
     $.expr[':'].external = function (obj) {
         return !(obj.href == "")
